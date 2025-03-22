82dd49f791872b70c77fa89327f8412bd1e8f486
Narita Haku <bokong8119@163.com>
2025-03-22 18:02:41 +0800

    Update 撒旦推杆（Satanic Yoke）核心解密程序.py

--- 撒旦推杆（Satanic Yoke）核心解密程序.py
+++ 撒旦推杆（Satanic Yoke）核心解密程序.py
@@ -3,7 +3,7 @@ answer=''
 search_range={}
 search={'A':3,'B':1,'C':2,'D':2}
 def create_key(search=search,range_index=5):
-#
+#生成凯撒加密解密密钥
     for i in ['A','B','C','D']:
         temp_range=0
         temp=[]
@@ -12,7 +12,7 @@ def create_key(search=search,range_index=5):
             temp.append(chr(ord(temp[-1])+4))
             temp_range+=1
         search_range[i]=temp
-def change_return(answer,key):  #换位加密
+def change_return(answer,key):  #换位加密法反向解密
     temp=['' for k in range(len(answer))]
     for i in range(len(answer)):
         index_temp=(i+key)%len(answer)
@@ -21,19 +21,19 @@ def change_return(answer,key):  #换位加密
     for st in temp:   #列表元素整合
         output_temp+=st
     return output_temp
-def change_key_create(answer,time=3):   #换位加密密钥生成，time表示循环几次，time值课更改
+def change_key_create(answer,time=3):   #换位密钥
     time_temp=0
     while time_temp<time:
-        temp=int(input('你想移动几位'))
+        temp=int(input('你想移动几位'))  #从左往右
         if abs(temp)>=len(answer):  #优化
             temp=temp%len(answer)
         time_temp+=1
         change_key.append(temp)
 def change_return_all(answer):
-    for i in range(len(change_key)-1,-1,-1):
-        answer=change_return(answer,-(change_key[i]))
+    for i in range(len(change_key)-1,-1,-1): #倒过来
+        answer=change_return(answer,-(change_key[i])) #有负号
     return answer
-def return_judge(answer):
+def return_judge(answer):  #凯撒加密法反向解密
     temp_all=''
     list_temp=['A','B','C','D']
     for j in range(len(answer)):
@@ -42,7 +42,7 @@ def return_judge(answer):
                 temp_all+=i
                 break
         else:
-            return "WRONG"
+            return "答案或密钥有误，请重新输入"
     return temp_all
 def unify_all(answer): #格式统一为大写
     temp=''
